b2e8572003cfe278910c9b9d87498e530fff585f
Jacob Sendzik <jake.sendzik@gmail.com>
2023-12-06 13:09:22 -0500

    Adds pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home.js'
 
 function App() {
   return (
+    <>
+    <header>
+      NavBar
+    </header>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      2023 Ford F150 Models
+      <Home />
     </div>
+    </>
   );
 }
 
