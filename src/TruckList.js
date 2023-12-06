d709de3fcb2e72b78d5065572ecfe40b2273fdd2
Jacob Sendzik <jake.sendzik@gmail.com>
2023-12-06 13:51:19 -0500

    adds get request and posts trucks to home page

--- src/TruckList.js
+++ src/TruckList.js
@@ -1,15 +1,29 @@
 import React from "react";
+import { useState } from "react";
 
 function TruckList () {
+
+    const [trucks, setTrucks] = useState([])
+
+    function getTrucks() {
+        fetch("http://localhost:3000/trucks")
+        .then(r => r.json())
+        .then((res)=> setTrucks(res))
+    }
+
     return (
         <div>
-            Truck List
-            1
-            2
-            3
-            4
-            5
-            6
+            <button onClick={getTrucks}>Show Trucks</button>
+            {trucks.map((truck) => {
+                return (
+                    <p>
+                        <h3>{truck.name}</h3>
+                        <img src={truck.image}/>
+                        <label>Starting Price: ${truck.price}</label>
+                    </p>
+
+                )
+            })}
         </div>
     )
 }
