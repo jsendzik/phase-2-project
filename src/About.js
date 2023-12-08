f7e19ca67a8f58be96c4fce1f458c70dadf526f6
Jacob Sendzik <jake.sendzik@gmail.com>
2023-12-08 13:40:27 -0500

    adds navbar to about

--- src/About.js
+++ src/About.js
@@ -1,10 +1,14 @@
 import React from "react";
+import NavBar from "./NavBar";
 
 function About () {
     return (
         <div>
-            <header>About Page</header>
-            <p>This site compares and provides information on 2023 Ford F150 models.</p>
+            <header>
+                <NavBar />
+            </header>
+            <h1>About</h1>
+            <p>This site compares and provides information on 2023 Ford F150 models.  This application was made by Jacob Sendzik as a project for the Flatiron School.  This has no affiliation to Ford.  All credit for images and information goes to Ford.</p>
         </div>
     )
 }
