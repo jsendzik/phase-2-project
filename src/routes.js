31608ec551148db68f5950e20b6f29f79435ee93
Jacob Sendzik <jake.sendzik@gmail.com>
2023-12-08 13:07:11 -0500

    adds routes

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import Home from "./Home";
+import About from "./About";
+
+const routes = [
+    {
+        path: "/",
+        element: <Home />
+    },
+    {
+        path: "/about",
+        element: <About />
+    },
+]
+
+export default routes
