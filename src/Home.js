1c0c4ba9c2fe316e39b23e9cdf4f791549bc8cef
Jacob Sendzik <jake.sendzik@gmail.com>
2023-12-08 14:42:25 -0500

    adds styling in css

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,19 @@
 import React from "react";
-import TruckList from "./TruckList";
 import NavBar from "./NavBar";
+import "./index1.css"
+
 
 function Home () {
     return (
-        <div>
+        <div className="home">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Ford_logo_flat.svg/2560px-Ford_logo_flat.svg.png" alt="ford" width={200}/>
             <header>
                 <NavBar />
             </header>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Ford_logo_flat.svg/2560px-Ford_logo_flat.svg.png" alt="ford" width={100}/>
-            <h1>Home</h1>
-            <h2>2023 Ford F150</h2>
-            <p>Click the Models link above to view models.</p>
+            
+            <h1 className="title">Home</h1>
+            <h2 className="title">2023 Ford F150</h2>
+            <p className="wpar">Click the Models link above to view models.</p>
             
         </div>
         )
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,25 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./NavBar.css"
+import "./index1.css"
 
 function NavBar () {
     return (
         <nav>
             <NavLink
              to="/"
-             className={nav-link}>
+             className="nav-link">
                 Home
              </NavLink>
 
              <NavLink
              to="/about"
-             className={nav-link}>
+             className="nav-link">
                 About
              </NavLink>
 
              <NavLink
              to="/models"
-             className={nav-link}>
+             className="nav-link">
                 Models
              </NavLink>
         </nav>
