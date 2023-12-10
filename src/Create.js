db1e712579055f7633989cf6894afc1e03141100
Jacob Sendzik <jake.sendzik@gmail.com>
2023-12-10 12:53:49 -0500

    creates form on create page

--- src/Create.js
+++ src/Create.js
@@ -7,9 +7,17 @@ function Create () {
             <header>
                 <NavBar />
             </header>
-            <form>
-                <input />
-                
+            <h1 className="title">Create</h1>
+            <form className="form">
+                <div>
+                    <label>Truck Model Name:</label>
+                    <input id="name" type="text" placeholder="Example: XLT, Lariat, ..etc"/>
+                </div>
+                <div>
+                    <label>Starting Price:</label>
+                    <input id="price" type="text" placeholder="Example: $29,999" />
+                </div>
+                <button type="submit">Create Truck</button>
             </form>
         </div>
     )
